256299803c313965015f5e14c928b634daa0aa42
Ron Dronet <ron@dronetdevdesign.com>
2022-07-03 14:00:19 -0500

    combining search and data from API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,69 @@
-var apiUrl = "https://api.openweathermap.org/data/2.5/weather?appid=e0c79796705c396d02d89d49c8fdba43";
- // added dynamic value
+// original URL:
 var cityUrl = "https://api.openweathermap.org/geo/1.0/direct?limit=5&appid=e0c79796705c396d02d89d49c8fdba43";
-
+var key = "e0c79796705c396d02d89d49c8fdba43";
+var city = "Houston";
+var currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+// console.log(currentWeatherUrl);
+// main section result goes here:
+var mainResultEl = document.querySelector("#main-section-result");
+// search button
 var searchForm = document.getElementById("user-search");
+
+// get weather data:
 var onSearchSubmit = function(event) {
   event.preventDefault();
 
-var fData = new FormData(event.target);
+    fetch(currentWeatherUrl)
+      .then(function(response) {
+      return response.json();
+  })
+      .then(function(data) {
+        console.log(data.coord);
 
-var city = fData.get("cityname");
-  fetch(cityUrl + "&q=" + city) 
-  .then(function(response) {
-    return response.json();
+  var lat = data.coord.lat;
+  var lon = data.coord.lon;
+  var fiveDayForecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`;
+    fetch(fiveDayForecastUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+    });
+
+  var fData = new FormData(event.target);
+
+  var city = fData.get("cityname");
+    fetch(cityUrl + "&q=" + city) 
+      .then(function(response) {
+      return response.json();
+    })
+      .then(function(response) {
+      return response[0];
+    })
+      .then(console.log);
   })
-  .then(function(response) {
-    return response[0];
-  })
-  .then(console.log);
 }
 
 searchForm.addEventListener("submit", onSearchSubmit);
+ 
+
+
+
+
+// var searchForm = document.getElementById("user-search");
+// var onSearchSubmit = function(event) {
+//   event.preventDefault();
+
+// var fData = new FormData(event.target);
+
+// var city = fData.get("cityname");
+//   fetch(cityUrl + "&q=" + city) 
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(response) {
+//     return response[0];
+//   })
+//   .then(console.log);
+// }
+
+// searchForm.addEventListener("submit", onSearchSubmit);
